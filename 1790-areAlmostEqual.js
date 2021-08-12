55cad85d1589e768dbd0bbfcfef726944f968d1e
Yosure <1939210266@qq.com>
2021-08-12 22:46:56 +0800

    2021-8-12

---
+++ 1790-areAlmostEqual.js
@@ -0,0 +1,13 @@
+var s1 = 'bank',s2 = 'kanb'
+var areAlmostEqual = function(s1,s2){
+    if(s1 === s2) return true;
+    let diff = []
+    let i = s1.length
+    while(i--){
+        if(s1.charAt(i) !== s2.charAt(i)){
+            diff.push([s1.charAt(i),s2.charAt(i)])
+        }
+    }
+    return diff.length === 2 && diff[0][0]===diff[1][1] && diff[0][1]===diff[1][0]
+}
+console.log(areAlmostEqual(s1,s2))
